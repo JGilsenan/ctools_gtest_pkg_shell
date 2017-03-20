7733fb7f31d9491b05e9523f26dbf2bf2347bca8
Joe Gilsenan <josephpgilsenan@gmail.com>
2017-03-20 12:07:21 -0400

    Added structure for using rostest for integration tests

--- src/test/utest.cpp
+++ src/test/utest.cpp
@@ -8,6 +8,11 @@ TEST(PackageShellTestPlaceholder, emptyTest)
 	EXPECT_EQ(1, 1);
 }
 
+TEST(PackageShellTestPlaceholder, emptyTestTwo)
+{
+  EXPECT_EQ(1, 1);
+}
+
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
